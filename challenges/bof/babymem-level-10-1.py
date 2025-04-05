1acff4e305c27a459e7e0dde073861ba806c3922
Ernesto Pastori <43282001+ernepasto@users.noreply.github.com>
2025-04-05 20:11:31 +0200

    Update babymem-level-10-1.py

--- challenges/bof/babymem-level-10-1.py
+++ challenges/bof/babymem-level-10-1.py
@@ -3,7 +3,13 @@ from pwn import *
 
 '''
 -- OBIETTIVO DEL PROGRAMMA --
-
+In questa challenge è attivo il canary, pertanto non è possibile eseguire un attacco di buffer overflow come nelle precedenti.
+Tuttavia la flag viene letta dal programma prima di acquisire l'input dall'utente e l'input acquisito viene poi stampato.
+Quindi, è possibile far stampare al programma il contenuto della memoria successivo al buffer dove viene salvato l'input acquisito,
+fornendo semplicemente come input una stringa senza il carattere di terminazione '\0'. Il programma stamperà la stringa, ma non essendoci 
+il carattere di terminazione proseguirà stampando il contenuto della memoria, dove si trova la flag.
+Per procedere con l'attacco si fornisce al programma una stringa di dimensione pari alla distanza tra l'inizio del buffer e la flag,
+dato che la memoria successiva è occupata, non viene inserito in automatico '\0' alla fine della stringa. 
 -----------------------------
 '''
 

